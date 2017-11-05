3a62c7534ac7b5f8140b19d2a0cb44c7bb3b9e79
Eric <epurdy@uchicago.edu>
2017-11-05 01:46:45 -0700

    Got backwards Euler working on simple circuits

--- circuit.py
+++ circuit.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from collections import defaultdict
 import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
@@ -12,6 +13,10 @@ def get_next_id(typ):
     print('ID', rv)
     return rv
 
+def indent_str(s, n=2):
+    lines = s.split('\n')
+    lines = [' ' * n + x for x in lines]
+    return '\n'.join(lines)
 
 class MnaEquation(object):
     def __init__(self, A, B, c, rev_vdict, rev_idict):
@@ -23,18 +28,61 @@ class MnaEquation(object):
         self.nv = len(self.rev_vdict)
         self.ni = len(self.rev_idict)
 
-    def simulate(self, tmax, dt):
-        x, _, _, _ = np.linalg.lstsq(self.A, self.c)
-        xs = [x]
-        t = 0.0
-        while t < tmax:            
-            dx, _, _, _ = np.linalg.lstsq(self.B, self.c - self.A.dot(x))
-            print dx
-            x += dt * dx
-            xs.append(x)
-            t += dt
+        self.rev_dict = {}
+        for k in self.rev_vdict:
+            self.rev_dict[k] = self.rev_vdict[k]
+        for k in self.rev_idict:
+            self.rev_dict[k] = self.rev_idict[k]
 
-        return np.array(xs)
+    def __repr__(self):
+        Astr = indent_str(str(self.A), 14)
+        Bstr = indent_str(str(self.B), 14)
+        Binvstr = indent_str(str(np.linalg.pinv(self.B)), 14)
+        cstr = indent_str(str(self.c), 14)
+        return """
+        A x + B x' = c
+          A = 
+{}
+          B = 
+{}
+          Binv = 
+{}
+          c = 
+{}
+        """.format(Astr, Bstr, Binvstr, cstr)
+
+    def simulate_be(self, tmax, dt, x0=None, vars=None):
+        if x0 is None:
+            x, res, rank, _ = np.linalg.lstsq(self.A, self.c)
+            print res, rank
+            print x
+            print 'residual', self.c - self.A.dot(x)
+            print 'residual', self.c - self.A.dot([0, 9, -9, 9, 9])
+        else:
+            x = np.array(x0)
+
+        if vars is None:
+            indices = range(self.nv + self.ni)
+        else:
+            indices = [self.rev_dict[k] for k in vars]
+
+        xs = [x[indices]]
+        t = 0.0
+        n = self.nv + self.ni
+        mat = self.A + self.B / dt
+
+        while t < tmax:
+            x, _, _, _ = np.linalg.lstsq(mat, self.c + (1.0/dt) * self.B.dot(x))
+            xs.append(list(x[indices]))
+            t += dt
+            #print x
+            #print xdx
+
+        xs = np.array(xs)
+        print xs.shape
+        print xs
+
+        return xs
 
 class Circuit(object):
     def __init__(self):
@@ -47,6 +95,16 @@ class Circuit(object):
         # add ground
         self._ground = get_next_id('node')
         self.add_node(self._ground)
+
+    def draw(self):
+        pos = nx.drawing.layout.spring_layout(self.graph)
+        edge_labels = defaultdict(lambda: '')
+        for edge in self.graph.edges():
+            edge_data = self.graph.get_edge_data(*edge)
+            for idx in edge_data:
+                edge_labels[edge] += edge_data[idx]['data']
+        nx.draw_networkx(self.graph, pos, with_labels=True)
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels)
         
     def add_node(self, node):
         self.graph.add_node(node)
@@ -172,6 +230,8 @@ class TwoNodeCircuitComponent(CircuitComponent):
 
     
 class Resistor(TwoNodeCircuitComponent):
+    """A resistor without an explicit current variable.
+    """
     def __init__(self, crkt, resistance):
         self.id = get_next_id('R')
         super(Resistor, self).__init__(crkt)
@@ -187,10 +247,34 @@ class Resistor(TwoNodeCircuitComponent):
             (self.positive, 'v', self.negative, 'v', -1.0 / self.resistance),
             (self.negative, 'v', self.positive, 'v', -1.0 / self.resistance),
             (self.negative, 'v', self.negative, 'v',  1.0 / self.resistance),
-            ]
+        ]
+
+
+class Resistor2(TwoNodeCircuitComponent):
+    """A resistor with an explicit current variable.
+    """
+    def __init__(self, crkt, resistance):
+        self.id = get_next_id('R')
+        super(Resistor2, self).__init__(crkt)
+        self.resistance = float(resistance)
+
+    @property
+    def has_current_variable(self):
+        return True
+
+    def Astamp(self):
+        return [
+            (self.positive, 'v', self.id, 'i',  1),
+            (self.negative, 'v', self.id, 'i', -1),
+            (self.id, 'i', self.positive, 'v',  1),
+            (self.id, 'i', self.negative, 'v', -1),
+            (self.id, 'i', self.id, 'i',  -self.resistance),
+        ]
 
 
 class Capacitor(TwoNodeCircuitComponent):
+    """A capacitor without an explicit current variable.
+    """
     def __init__(self, crkt, capacitance):
         self.id = get_next_id('C')
         super(Capacitor, self).__init__(crkt)
@@ -206,10 +290,42 @@ class Capacitor(TwoNodeCircuitComponent):
             (self.positive, 'v', self.negative, 'v', -self.capacitance),
             (self.negative, 'v', self.positive, 'v', -self.capacitance),
             (self.negative, 'v', self.negative, 'v',  self.capacitance),
-            ]
+        ]
+
+
+class Capacitor2(TwoNodeCircuitComponent):
+    """A capacitor with an explicit current variable.
+    """
+    def __init__(self, crkt, capacitance):
+        self.id = get_next_id('C')
+        super(Capacitor2, self).__init__(crkt)
+        self.capacitance = float(capacitance)
+
+    @property
+    def has_current_variable(self):
+        return True
+
+    def Astamp(self):
+        return [
+            # these do not appear in Najm but seem to be necessary
+            (self.positive, 'v', self.id, 'i',  1),
+            (self.negative, 'v', self.id, 'i', -1),
+            (self.id, 'i', self.positive, 'v',  1),
+            (self.id, 'i', self.negative, 'v', -1),
+
+            # this one does appear in Najm
+            (self.id, 'i', self.id, 'i',  1),
+        ]
+
+    def Bstamp(self):
+        return [
+            (self.id, 'i', self.positive, 'v',  self.capacitance),
+            (self.id, 'i', self.negative, 'v', -self.capacitance),
+        ]
 
     
 class VoltageSource(TwoNodeCircuitComponent):
+    """Voltage sources are required to have an explicit current variable."""
     def __init__(self, crkt, voltage):
         self.id = get_next_id('V')
         super(VoltageSource, self).__init__(crkt)
--- test_circuit.py
+++ test_circuit.py
@@ -1,5 +1,10 @@
-import circuit
+import numpy as np
+import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
+import circuit
 
 
 def test_circuit_resistance():
@@ -11,32 +16,51 @@ def test_circuit_resistance():
     print crkt.graph.edges()
     mna = crkt.assemble_mna_equation()
     stuff = mna.simulate(10.0, 0.1)
-    print stuff
+
+    plt.savefig('crkt1.png')
 
     
 def test_circuit_capacitance():
     crkt = circuit.Circuit()
     battery = circuit.VoltageSource(crkt, 9)
     resistor = circuit.Resistor(crkt, 1)
-    capacitor = circuit.Capacitor(crkt, 1e-6)  # 1 microFarad
-    battery.negative = resistor.positive
+    capacitor = circuit.Capacitor(crkt, 1e-1)
+    battery.positive = resistor.positive
     resistor.negative = capacitor.positive
-    capacitor.negative = battery.positive = crkt._ground
+    capacitor.negative = battery.negative = crkt._ground
 
     mna = crkt.assemble_mna_equation()
-    stuff = mna.simulate(0.01, 0.001)
-    print mna.rev_vdict
-    print mna.rev_idict
+    stuff = mna.simulate_be(1e0, 1e-3, x0=[0, 9, 0], vars=['node_7', 'node_3'])
+
+    plt.plot(stuff)
+    plt.savefig('cap.png')
+
+    plt.figure()
+    crkt.draw()
+    plt.savefig('crkt2.png')
 
 def test_circuit_parallel_resistor_capacitor():
     crkt = circuit.Circuit()
-    battery = crky.VoltageSource(9)
-    resistor = crkt.Resistor(1)
-    capacitor = crky.Capacitor(1e-6)  # 1 microFarad
+    battery = circuit.VoltageSource(crkt, 9)
+    resistor = circuit.Resistor(crkt, 1)
+    capacitor = circuit.Capacitor(crkt, 1e-1)  # 1 microFarad
     battery.negative = resistor.positive = capacitor.positive
-    battery.positive = resistor.negative = capacitor.negative
+    battery.positive = resistor.negative = capacitor.negative = crkt._ground
+
+    mna = crkt.assemble_mna_equation()
+    stuff = mna.simulate_be(1e0, 1e-3, x0=[9, 0], vars=['V_2'])
+
+    print mna.rev_idict
+
+    plt.plot(stuff)
+    plt.savefig('cap_par.png')
+
+    plt.figure()
+    crkt.draw()
+    plt.savefig('crkt3.png')
 
 
 if __name__ == '__main__':
     #test_circuit_resistance()
-    test_circuit_capacitance()
+    #test_circuit_capacitance()
+    test_circuit_parallel_resistor_capacitor()
