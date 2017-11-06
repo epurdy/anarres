abce734ae49ddfa04485dc7b33333648ce6db13e
Eric <epurdy@uchicago.edu>
2017-11-05 19:35:07 -0800

    Halfway through cleanup

---
+++ util.py
@@ -0,0 +1,11 @@
+"""Miscellaneous useful functions.
+"""
+
+
+NEXT_ID = 0
+def get_next_id(typ):
+    global NEXT_ID
+    rv = '%s_%06d' % (typ, NEXT_ID)
+    NEXT_ID += 1
+    print('ID', rv)
+    return rv
